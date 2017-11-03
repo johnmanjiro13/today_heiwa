8a9cfdbe020f96a62c028f1ec9bc2ffd29aa7855
johnmanjiro13 <juntama193@gmail.com>
2017-11-03 17:03:28 +0900

    fixed error

--- twpy.py
+++ twpy.py
@@ -1,6 +1,6 @@
 import tweepy
 import os
 
-auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], s.environ['CONSUMER_SECRET'])
+auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
 auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_SECRET'])
 api = tweepy.API(auth)
